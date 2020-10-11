292fdc74d426b3ea2765d0f473b5fa04d3a68245
lalit.nailwal@ebix.com <lalit.nailwal@ebix.com>
2020-10-11 11:57:11 +0530

    shown product component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent
+  },
+  {
+  path:'products',
+  pathMatch:'full',
+  loadChildren: './products/products.module#ProductsModule'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
