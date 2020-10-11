bdec260b0aa31a68a7b9811409aa942277479d48
lalit.nailwal@ebix.com <lalit.nailwal@ebix.com>
2020-10-11 15:20:00 +0530

    integrated the toolbar from angular material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,11 @@ const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: 'home',
+    pathMatch: 'full',
     component: HomeComponent
   },
   {
