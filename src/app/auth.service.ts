6ec09f31d77dbec76f92ff0a8e94821362f23960
lalit.nailwal@ebix.com <lalit.nailwal@ebix.com>
2020-10-15 20:38:38 +0530

    integration of http in auth service

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { of, Subject } from 'rxjs';
+import { of, Subject, throwError } from 'rxjs';
+import { catchError, switchMap } from 'rxjs/operators';
 import { User } from './user';
 
 
@@ -11,8 +13,8 @@ import { User } from './user';
 export class AuthService { 
 
   private user$ = new Subject<User>();
-
-  constructor() { }
+  private apiUrl = '/api/auth/';
+  constructor(private httpClient: HttpClient) { }
 
   login(email: string, password: string) {
     const loginCredential = {email, password};
@@ -31,11 +33,25 @@ export class AuthService {
   }
 
   register(user: any) {
-    //make an api call to save user in db
-    //update the user subject
-    this.setuser(user);
-    console.log(`registered user successfully`, user);
-    return of(user);
+    // //make an api call to save user in db
+    // //update the user subject
+    // this.setuser(user);
+    // console.log(`registered user successfully`, user);
+    // return of(user);
+
+
+    return this.httpClient.post<User>(`${this.apiUrl}register`, user).pipe
+    (
+      switchMap(savedUser => {
+        this.setuser(savedUser);
+        console.log(`user registerd successfully`, savedUser);
+        return of(savedUser);
+      }),
+      catchError(e => {
+        console.log(`Server error occured`, e)
+        return throwError(`Registeration failed please contact admin`);
+      })
+    );
   }
 
   private setuser(user){
